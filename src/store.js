cb248716b5c80fc85553185b53251b30f73cc20a
Anna Tasheva <anna.tasheva90@gmail.com>
2022-07-12 19:12:08 +0100

    Removed unused Redux store

--- src/store.js
+++
@@ -1,7 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import subredditReducer from "./features/subreddit/subredditSlice.js";
-
-
-export const store = configureStore({
-    reducer: { subreddit: subredditReducer }
-});
