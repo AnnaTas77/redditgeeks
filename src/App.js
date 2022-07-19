3708eb702629c58a04a4bac1ae4c1d21bbaf9afd
Anna Tasheva <anna.tasheva90@gmail.com>
2022-07-19 17:32:47 +0100

    Updated the add-new-subreddit functionality. Updated the CSS styling of the add button.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
 import React, { useState } from 'react';
-import { Article, Navbar, Search, Subreddit, Addsub } from './components';
+import { git Navbar, Subreddit, Addsub } from './components';
 import './App.css';
 
 function App() {
 
-  const [componentList, setComponentList] = useState([]);
+  const [updateTrigger, setUpdateTrigger] = useState(0);
 
-  const addSubbreddit = () => {
-    console.log('Clicked add new sub');
-    setComponentList(componentList.concat(<Subreddit key={componentList.length} />));
-  }
-
-
-
+  const initialSubreddits = JSON.parse(localStorage.getItem("favouriteSubs"));
   if (localStorage.getItem("favouriteSubs") === null) {
     localStorage.setItem("favouriteSubs", JSON.stringify(["frontend", "webdev", "programming"]))
   }
 
-  const initialSubreddits = JSON.parse(localStorage.getItem("favouriteSubs"));
 
+  const addSubbreddit = () => {
+
+    const storedSubs = localStorage.getItem("favouriteSubs");
+
+    if (storedSubs === null) {
+      console.error("State is not expected to be null at this point.");
+    }
+
+    let subsArray = JSON.parse(storedSubs);
+    subsArray.push("sofia")
+    localStorage.setItem("favouriteSubs", JSON.stringify(subsArray))
+
+    setUpdateTrigger(updateTrigger => updateTrigger + 1); // updates the state to force render
+  }
 
   return (
     <div className="app-container">
@@ -33,12 +40,9 @@ function App() {
             return <Subreddit key={index} localStorageIndex={index} initialSubreddit={initialSubreddit} />
           })}
 
-          {componentList}
-
           <div className='add-subreddit'>
             <Addsub addSubbreddit={addSubbreddit} />
           </div>
-
 
         </div>
 
